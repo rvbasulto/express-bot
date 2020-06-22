e173dc3da7088276ffa01eba6960f4ec27dbf20d
unknown <roberto.vbasulto@PC-Robertocvb.reduc.edu.cu>
2020-06-22 11:10:48 -0400

    Analizar el tipo de evento del webhook

--- server.js
+++ server.js
@@ -7,15 +7,15 @@
 
 // Imports dependencies and set up http server
 const express = require("express"),
-  bodyParser = require("body-parser"),
-  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
-  app = express().use(bodyParser.json()); // creates express http server
+    bodyParser = require("body-parser"),
+    PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
+    app = express().use(bodyParser.json()); // creates express http server
 
 // our default array of dreams
 const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
+    "Find and count some sheep",
+    "Climb a really tall mountain",
+    "Wash the dishes"
 ];
 
 // make all the files in 'public' available
@@ -24,70 +24,78 @@ app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+    response.sendFile(__dirname + "/views/index.html");
 });
 
 // send the default array of dreams to the webpage
 app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+    // express helps us take JS objects and send them as JSON
+    response.json(dreams);
 });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
 
 /*****extremo del webhook**********/
 // Creates the endpoint for our webhook
 app.post("/webhook", (req, res) => {
-  let body = req.body;
+    let body = req.body;
 
-  // Checks this is an event from a page subscription
-  if (body.object === "page") {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+    // Checks this is an event from a page subscription
+    if (body.object === "page") {
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
 
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log("Sender PSID: " + sender_psid);
-    });
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log("Sender PSID: " + sender_psid);
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED");
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send("EVENT_RECEIVED");
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 });
 
 /****************verificacion del webhook******************/
 // Adds support for GET requests to our webhook
 app.get("/webhook", (req, res) => {
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.Verify_TOKEN;
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = process.env.Verify_TOKEN;
 
-  // Parse the query params
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
+    // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+        // Checks the mode and token sent is correct
+        if (mode === "subscribe" && token === VERIFY_TOKEN) {
+            // Responds with the challenge token from the request
+            console.log("WEBHOOK_VERIFIED");
+            res.status(200).send(challenge);
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
-  }
 });
 
 // Handles messages events
